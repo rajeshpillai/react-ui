0a83ef74eaefe142ba3e6a979a92e2ee04a642e5
Rajesh Pillai <pillai.rajesh@gmail.com>
2017-07-19 15:30:54 +0530

    A simple table component with data binding

---
+++ src/components/Table/Table.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import  './table.css';
+ 
+/** A simple table with sorting, searching feature */
+class Table extends React.Component {
+   constructor (props) {
+       super(props);
+       console.log("CTOR: ", this.props);
+       this.state = {
+           data: [],
+           headers: []
+       }
+   } 
+   getCells(row) {
+     var cells = row.map((cell, index) => {
+        return <td key={index}>{cell}</td>
+     });
+
+     return cells;
+   }
+
+   render() {
+       console.log("RENDER: ", this.props);
+       
+       var thead = this.props.headers.map ((title, index) => {
+          return <th key={index}>{title}</th>
+       });  
+        
+       var tbody = this.props.initialData.map ((row, index) => {
+          return (
+            <tr key={index}>
+                {this.getCells(row)}
+            </tr>
+          );
+       });
+
+      
+       return (
+        <table>
+            <thead>
+                <tr>
+                    {thead}
+                </tr>
+            </thead>
+            <tbody>
+                {tbody}
+            </tbody>
+        </table>
+    );
+   }
+}
+export default Table;
---
+++ src/docs/examples/Table/ExampleSimpleTable.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Table from 'zs-react/Table';
+
+/** SimpleTable */
+export default class ExampleSimpleTable extends React.Component {
+    constructor () {
+       super();
+       this.headers = [
+        "Book", "Author", "Language", "Published", "Sales"
+       ];
+
+       this.data = [
+        ["The Lord of the Rings", "J. R. R. Tolkien", "English", "1954–1955", "150 million"], 
+        ["Le Petit Prince (The Little Prince)", "Antoine de Saint-Exupéry", "French", "1943", "140 million"], 
+        ["Harry Potter and the Philosopher's Stone", "J. K. Rowling", "English", "1997", "107 million"], 
+        ["And Then There Were None", "Agatha Christie", "English", "1939", "100 million"], 
+        ["Dream of the Red Chamber", "Cao Xueqin", "Chinese", "1754–1791", "100 million"], 
+        ["The Hobbit", "J. R. R. Tolkien", "English", "1937", "100 million"], 
+        ["She: A History of Adventure", "H. Rider Haggard", "English", "1887", "100 million"],
+      ];
+   }
+   render() {
+    return (
+        <Table headers = {this.headers} initialData={this.data} />
+    )
+  }
+}
